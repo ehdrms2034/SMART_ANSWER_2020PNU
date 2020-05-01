abcc5e3bc6c83d485ce4fa9143044f5ce1925a90
KimDongGeun <ehdrms2034@naver.com>
2020-05-01 20:20:51 +0900

    테스팅 환경 구축 및 간단한 테스트

--- Backend/src/src/test/java/com/smartanswer/ocrproject/ApplicationTests.java
+++ Backend/src/src/test/java/com/smartanswer/ocrproject/ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.smartanswer.ocrproject;
 
+import com.smartanswer.ocrproject.model.Member;
+import com.smartanswer.ocrproject.service.MemberService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private MemberService memberService;
+
 	@Test
 	void contextLoads() {
+		memberService.createMember(new Member("hellos","username","hello"));
 	}
 
 }
