ad58555fc1805c7f8491d3c490034ddb2d38522c
jaeseok3 <tjr3117@gmail.com>
2020-05-06 16:53:43 +0900

    aa

--- Backend/src/src/main/java/com/smartanswer/ocrproject/repository/AnswerRepository.java
+++ Backend/src/src/main/java/com/smartanswer/ocrproject/repository/AnswerRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface AnswerRepository extends MongoRepository<Answer,Long> {
-
+    Answer findByIdAndDate(String id, Date date);
 }
 
