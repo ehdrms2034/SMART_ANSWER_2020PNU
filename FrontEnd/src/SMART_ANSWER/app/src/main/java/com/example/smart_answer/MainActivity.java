d58815b3968534b650f559322427304e32b074b7
LeeGwangYong <leeky0929@naver.com>
2020-05-11 19:05:12 +0900

    홈 화면
    홈 화면 xml

--- FrontEnd/src/SMART_ANSWER/app/src/main/java/com/example/smart_answer/MainActivity.java
+++ FrontEnd/src/SMART_ANSWER/app/src/main/java/com/example/smart_answer/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.smart_answer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.smart_answer.ui.dashboard.DashboardFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentManager manager;
+    FragmentTransaction transaction;
+    BottomNavigationView navView;
+    Button dashboardBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        //fragment manage 설정
+        manager = getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+
+        navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -23,6 +39,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+        /*dashboardBtn = (Button)findViewById(R.id.resultBtn);
+        dashboardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment dashboardFragment = new DashboardFragment();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, dashboardFragment);
+                transaction.addToBackStack("fragment");
+                transaction.commit();
+            }
+        });*/
+    }
+    public BottomNavigationView getNavView(){
+        return navView;
+    }
+    /*public void replaceFragment(@NonNull Fragment fragment) {
+        //fragment stack에 기존에 있던 fragment 제거
+        manager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        transaction = manager.beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, fragment);
+        transaction.commit();
+    }*/
 }
--- FrontEnd/src/SMART_ANSWER/app/src/main/java/com/example/smart_answer/ui/home/HomeFragment.java
+++ FrontEnd/src/SMART_ANSWER/app/src/main/java/com/example/smart_answer/ui/home/HomeFragment.java
@@ -4,30 +4,65 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.example.smart_answer.MainActivity;
 import com.example.smart_answer.R;
+import com.example.smart_answer.ui.dashboard.DashboardFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        btnClick((Button)root.findViewById(R.id.resultBtn),1);
+        btnClick((Button)root.findViewById(R.id.scoreBtn),2);
+        btnClick((Button)root.findViewById(R.id.messageBtn),3);
+        btnClick((Button)root.findViewById(R.id.notificationBtn),4);
+        /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+        manager = getActivity().getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+
+
         return root;
+    }
+    private void replaceFragment(@NonNull Fragment fragment) {
+        transaction = manager.beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, fragment);
+        transaction.addToBackStack("fragment");
+        transaction.commit();
+    }
+    private void btnClick(Button btn,final int page){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DashboardFragment dashboardFragment = new DashboardFragment();
+                Bundle bundle = new Bundle();
+                bundle.putInt("page",page);
+                dashboardFragment.setArguments(bundle);
+                ((MainActivity)getActivity()).getNavView().getMenu().getItem(0).setChecked(false);
+                ((MainActivity)getActivity()).getNavView().getMenu().getItem(1).setChecked(true);
+                replaceFragment(dashboardFragment);
+            }
+        });
     }
 }
