8d867ec24e01557d1e8660cb560a879544792b08
KimDongGeun <ehdrms2034@naver.com>
2020-05-27 17:35:13 +0900

    add Request Body Modell

---
+++ Backend/src/src/main/java/com/smartanswer/ocrproject/model/Request/RequestLogin.java
@@ -0,0 +1,13 @@
+package com.smartanswer.ocrproject.model.Request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestLogin {
+
+    private String username;
+    private String password;
+
+}
