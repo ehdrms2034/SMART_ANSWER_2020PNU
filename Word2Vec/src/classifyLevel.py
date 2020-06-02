39b7670ff6e65b1eed5ab2940cf4746caaf628d5
wslbal <ckdgus777@naver.com>
2020-06-02 20:05:19 +0900

    단어 출현 횟수별 분류 0602
    #4AAH-2

---
+++ Word2Vec/src/classifyLevel.py
@@ -0,0 +1,36 @@
+from gensim.models import KeyedVectors
+
+loaded_model = KeyedVectors.load_word2vec_format("eng_w2v_5.txt") # 모델 로드
+key_list5 = list(loaded_model.wv.vocab.keys())
+len(key_list5)
+
+loaded_model = KeyedVectors.load_word2vec_format("eng_w2v_7.txt") # 모델 로드
+key_list7 = list(loaded_model.wv.vocab.keys())
+len(key_list7)
+
+loaded_model = KeyedVectors.load_word2vec_format("eng_w2v_11.txt") # 모델 로드
+key_list11 = list(loaded_model.wv.vocab.keys())
+len(key_list11)
+
+loaded_model = KeyedVectors.load_word2vec_format("eng_w2v_20.txt") # 모델 로드
+key_list20 = list(loaded_model.wv.vocab.keys())
+len(key_list20)
+
+loaded_model = KeyedVectors.load_word2vec_format("eng_w2v_50.txt") # 모델 로드
+key_list50 = list(loaded_model.wv.vocab.keys())
+len(key_list50)
+
+loaded_model = KeyedVectors.load_word2vec_format("eng_w2v_500.txt") # 모델 로드
+key_list500 = list(loaded_model.wv.vocab.keys())
+len(key_list500)            #level 0
+
+key_list5_7 = [x for x in key_list5 if x not in key_list7]
+print(len(key_list5_7))     #level 5
+key_list7_11 = [x for x in key_list7 if x not in key_list11]
+print(len(key_list7_11))    #level 4
+key_list11_20 = [x for x in key_list11 if x not in key_list20]
+print(len(key_list11_20))   #level 3
+key_list20_50 = [x for x in key_list20 if x not in key_list50]
+print(len(key_list20_50))   #level 2
+key_list50_500 = [x for x in key_list50 if x not in key_list500]
+print(len(key_list50_500))  #level 1
--- Word2Vec/src/silhouette_score.py
+++ Word2Vec/src/silhouette_score.py
@@ -14,6 +14,7 @@ kmeans.fit(X)
 labels = kmeans.labels_
 centroids = kmeans.cluster_centers_
 
+'''
 print("Cluster id labels for inputted data")
 print(labels)
 print("Centroids data")
@@ -25,7 +26,7 @@ print(centroids[3])
 print(
     "Score (Opposite of the value of X on the K-means objective which is Sum of distances of samples to their closest cluster center):")
 print(kmeans.score(X))
-
+'''
 silhouette_score = metrics.silhouette_score(X, labels, metric='euclidean')
 
 print("Silhouette_score: ")
