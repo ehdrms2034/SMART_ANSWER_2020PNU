919974fbd40c7e862177065d1adc6ea8248dc889
uuwani <paulchoi9502@gmail.com>
2020-06-25 00:30:11 +0900

    나만의 단어장 구현 #4AAH-69

---
+++ iOS/HelpENG/MyVocaList/MyVocaListVC.swift
@@ -0,0 +1,159 @@
+//
+//  MyVocaList.swift
+//  HelpENG
+//
+//  Created by Wanni on 2020/06/24.
+//  Copyright © 2020 Wanni. All rights reserved.
+//
+
+import UIKit
+
+class MyVocaListVC:UIViewController {
+    
+    @IBOutlet weak var levelBtn: UIButton!
+    
+    @IBOutlet weak var categoryBtnOutsideView: UIView!
+    @IBOutlet weak var wrongVocaBtn: UIView!
+    @IBOutlet weak var recommendVocaBtn: UIView!
+    @IBOutlet weak var wrongVocaLabel: UILabel!
+    @IBOutlet weak var recommendVocaLabel: UILabel!
+    @IBOutlet weak var myVocaListTbOutsideView: UIView!
+    
+    let vocaListTableView: MyVocaListTableView = {
+        let tableView = MyVocaListTableView()
+        tableView.initUI()
+        return tableView
+    }()
+    
+    var vocaCategory:VocaCategory = .wrongVoca {
+        willSet(newValue) {
+            changeCategoryBtnUI(category: newValue)
+            vocaListTableView.category = newValue
+        }
+    }
+    
+    @IBAction func touchWrongVocaBtn(_ sender: Any) {
+        
+        if vocaCategory == .recommendVoca {
+            vocaCategory = .wrongVoca
+        }
+    }
+    
+    @IBAction func touchRecommendVocaBtn(_ sender: Any) {
+        print("touch")
+        if vocaCategory == .wrongVoca {
+            vocaCategory = .recommendVoca
+        }
+    }
+    
+    @IBAction func touchLevelBtn(_ sender: Any) {
+        if vocaListTableView.level == 5 {
+            vocaListTableView.level = 1
+            levelBtn.setTitle("level\(vocaListTableView.level)", for: .normal)
+        } else {
+            vocaListTableView.level += 1
+            levelBtn.setTitle("level\(vocaListTableView.level)", for: .normal)
+        }
+    }
+    
+    override func viewDidLoad() {
+        initTableView()
+        initCategoryBtnStackView()
+        initLevelBtn()
+        initDelegate()
+    }
+    
+    func initDelegate() {
+        vocaListTableView.dataSource = self
+        vocaListTableView.delegate = self
+    }
+    
+}
+
+//MARK: - MyVocaListVC setup View, setup categoryBtn
+extension MyVocaListVC: MyColor {
+    
+    func initTableView() {
+        myVocaListTbOutsideView.addSubview(vocaListTableView)
+        vocaListTableView.translatesAutoresizingMaskIntoConstraints = false
+        vocaListTableView.separatorStyle = .none
+        vocaListTableView.allowsSelection = false
+        vocaListTableView.topAnchor.constraint(equalTo: myVocaListTbOutsideView.topAnchor).isActive = true
+        vocaListTableView.bottomAnchor.constraint(equalTo: myVocaListTbOutsideView.bottomAnchor).isActive = true
+        vocaListTableView.rightAnchor.constraint(equalTo: myVocaListTbOutsideView.rightAnchor).isActive = true
+        vocaListTableView.leftAnchor.constraint(equalTo: myVocaListTbOutsideView.leftAnchor).isActive = true
+    }
+    
+    func initCategoryBtnStackView() {
+        categoryBtnOutsideView.layer.borderWidth = 1
+        categoryBtnOutsideView.layer.borderColor = #colorLiteral(red: 0.2030145526, green: 0.2030202448, blue: 0.203017205, alpha: 1)
+    }
+    
+    func initLevelBtn() {
+        levelBtn.layer.cornerRadius = 5
+        levelBtn.layer.borderWidth = 1
+        levelBtn.layer.borderColor = #colorLiteral(red: 0.2030145526, green: 0.2030202448, blue: 0.203017205, alpha: 1)
+    }
+    
+}
+
+extension MyVocaListVC {
+    
+    func changeCategoryBtnUI(category: VocaCategory) {
+        switch category {
+        case .wrongVoca:
+            wrongVocaBtn.backgroundColor = mainColor
+            wrongVocaLabel.textColor = .white
+            recommendVocaBtn.backgroundColor = .white
+            recommendVocaLabel.textColor = mainColor
+        case .recommendVoca:
+            recommendVocaBtn.backgroundColor = mainColor
+            recommendVocaLabel.textColor = .white
+            wrongVocaBtn.backgroundColor = .white
+            wrongVocaLabel.textColor = mainColor
+        }
+    }
+}
+
+extension MyVocaListVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let data = vocaListTableView.nowTbData else {
+            return 0
+        }
+        
+        return data[section].vocaData.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        guard let sectionNum = vocaListTableView.sectionNum else {
+            return 0
+        }
+        return sectionNum
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let data = vocaListTableView.nowTbData else {
+            let cell = UITableViewCell()
+            return cell
+        }
+        
+        switch indexPath.row {
+        case 0:
+            print("cellcellcell")
+            let cell = MyVocaListTbViewSectionCell()
+            cell.setDateLabelText(text: data[indexPath.section].date)
+            return cell
+        default:
+            print("cellcellcell")
+            let cell = TbViewVocaCell()
+            let (voca, meaning) = (data[indexPath.section].vocaData[indexPath.row-1].voca, data[indexPath.section].vocaData[indexPath.row-1].meaning)
+            cell.setVocaAndMeaningText(voca: voca, meaning: meaning)
+            return cell
+            
+        }
+    }
+    
+    
+}
--- iOS/HelpENG/VocaTest/ExpandingTbViewSectionCell.swift
+++ iOS/HelpENG/VocaTest/ExpandingTbViewSectionCell.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
+protocol ExpandingTbViewSectionCellProtocol {
+    func expanding(selectedIndexPath indexPath :IndexPath, completion: (_ isOpend:Bool)->())
+}
+
 class ExpandingTbViewSectionCell: UITableViewCell {
     
     let containerView = UIView()
     let dateLabel = UILabel()
     let stackView = ExpandingTbViewSectionStackView()
+    let expandingBtn = UIButton()
     
+    var indexPath: IndexPath?
+    var delegate: ExpandingTbViewSectionCellProtocol?
+    
+    var sectionIsOpened: Bool = false {
+        willSet(newValue) {
+            expandingBtnImageSetup(isOpened: newValue, duration: 0.0)
+        }
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -23,6 +36,7 @@ class ExpandingTbViewSectionCell: UITableViewCell {
         initContainerView()
         initTitleLabelView()
         initStackView()
+        initExpandingBtn()
     }
     
     override func awakeFromNib() {
@@ -42,7 +56,7 @@ extension ExpandingTbViewSectionCell: MyColor {
     
     func initContainerView() {
         self.contentView.addSubview(containerView)
-        containerView.backgroundColor = mainColor
+        containerView.backgroundColor = cellBackground
         containerView.layer.cornerRadius = 10
         containerView.translatesAutoresizingMaskIntoConstraints = false
         containerView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant:  10).isActive = true
@@ -58,7 +72,7 @@ extension ExpandingTbViewSectionCell: MyColor {
     func initTitleLabelView() {
         self.containerView.addSubview(dateLabel)
         dateLabel.backgroundColor = clearColor
-        dateLabel.textColor = textColor
+        dateLabel.textColor = .black
         dateLabel.translatesAutoresizingMaskIntoConstraints = false
         dateLabel.leftAnchor.constraint(equalTo: containerView.leftAnchor,
                                              constant: 10).isActive = true
@@ -78,6 +92,46 @@ extension ExpandingTbViewSectionCell: MyColor {
         stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10).isActive = true
         stackView.setupStackView()
         
+    }
+    
+    func initExpandingBtn() {
+        containerView.addSubview(expandingBtn)
+        expandingBtn.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(systemName: "arrowtriangle.down.fill")
+        expandingBtn.setImage(image?.withTintColor(mainColor, renderingMode: .alwaysOriginal)
+, for: .normal)
+        expandingBtn.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 1.0/4.0).isActive = true
+        expandingBtn.widthAnchor.constraint(equalTo: expandingBtn.heightAnchor, multiplier: 1.0).isActive = true
+        expandingBtn.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -10).isActive = true
+        expandingBtn.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        expandingBtn.addTarget(self, action: #selector(touchExpandingBtn), for: .touchUpInside)
+        
+    }
+    
+    func expandingBtnImageSetup(isOpened: Bool, duration: CGFloat = 0.4) {
+        switch isOpened {
+        case true:
+            UIView.animate(withDuration: TimeInterval(duration), animations: {
+                self.expandingBtn.transform = CGAffineTransform(rotationAngle: (180.0 * CGFloat(Double.pi)) / 180.0)
+            })
+        default:
+            UIView.animate(withDuration: TimeInterval(duration), animations: {
+                self.expandingBtn.transform = CGAffineTransform(rotationAngle: (0.0 * CGFloat(Double.pi)) / 180.0)
+            })
+        }
+//        isOpened ? expandingBtn.setImage(UIImage(systemName: "arrowtriangle.up.fill"), for: .normal) : expandingBtn.setImage(UIImage(systemName: "arrowtriangle.down.fill"), for: .normal)
+    }
+}
+
+extension ExpandingTbViewSectionCell {
+    
+    @objc func touchExpandingBtn() {
+        guard let indexPath = self.indexPath else {
+            return
+        }
+        self.delegate?.expanding(selectedIndexPath: indexPath,completion: {(isOpened) in
+            expandingBtnImageSetup(isOpened: isOpened)
+        })
     }
 }
 
